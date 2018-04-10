59a06ae24eb1c829ebe475ba681aeecc362c5329
Massimiliano Pesce <massimiliano.pesce@gmail.com>
2018-04-10 22:51:34 +0200

    changed order of the struct's members

--- pkg/input/game_controller.go
+++ pkg/input/game_controller.go
@@ -2,14 +2,14 @@ package input
 
 type GameController interface {
 	Connected() (bool)
+	Open(deviceIndex int) (bool)
+	Close()
+	Update()
 	NumButtons() (int)
 	NumAxis() (int)
 	ButtonPressed(buttonId int) (bool)
 	ButtonReleased(buttonId int) (bool)
 	ButtonDown(buttonId int) (bool)
-	Open(deviceIndex int) (bool)
-	Close()
-	Update()
 	GetAxisValue(axisIndex int) (float32)
 	GetAxisDigital(axisIndex int) (float32)
 }
