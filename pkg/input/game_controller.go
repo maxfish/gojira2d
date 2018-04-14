e41e7f666a28c064f0def8ce2ca414615de7c779
Massimiliano Pesce <massimiliano.pesce@gmail.com>
2018-04-14 09:26:20 +0200

    Constants for axes and buttons

--- pkg/input/game_controller.go
+++ pkg/input/game_controller.go
@@ -3,6 +3,31 @@ package input
 type ControllerAxis int
 type ControllerButton int
 
+const (
+	BUTTON_A ControllerButton = iota
+	BUTTON_B
+	BUTTON_X
+	BUTTON_Y
+	BUTTON_BACK
+	BUTTON_GUIDE
+	BUTTON_START
+	BUTTON_LEFT_STICK
+	BUTTON_RIGHT_STICK
+	BUTTON_LEFT_SHOULDER
+	BUTTON_RIGHT_SHOULDER
+	BUTTON_DIR_PAD_UP
+	BUTTON_DIR_PAD_DOWN
+	BUTTON_DIR_PAD_LEFT
+	BUTTON_DIR_PAD_RIGHT
+
+	AXIS_LEFT_X ControllerAxis = iota + 100
+	AXIS_LEFT_Y
+	AXIS_RIGHT_X
+	AXIS_RIGHT_Y
+	AXIS_TRIGGER_LEFT
+	AXIS_TRIGGER_RIGHT
+)
+
 type GameController interface {
 	Connected() bool
 	Open(deviceIndex int) bool
