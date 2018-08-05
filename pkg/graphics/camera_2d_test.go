e91c27f1054ebb1a4da1df5eaaed34a0c937e326
Massimiliano Pesce <massimiliano.pesce@gmail.com>
2018-08-05 16:14:31 +0200

    Removed checks on the matrixDirty internal var

--- pkg/graphics/camera_2d_test.go
+++ pkg/graphics/camera_2d_test.go
@@ -10,18 +10,12 @@ func TestCenteredCamera2D(t *testing.T) {
 	c := NewCamera2D(100, 100, 1, true)
 
 	c.SetPosition(10, 10)
-	if !c.matrixDirty {
-		t.Errorf("SetPosition failed to set matrix dirty to true")
-	}
 	expected := mgl32.Mat4FromRows(mgl32.Vec4{0.02, 0, 0, -0.2}, mgl32.Vec4{0, -0.02, 0, 0.2}, mgl32.Vec4{0, 0, 0.5, 0}, mgl32.Vec4{0, 0, 0, 1})
 	if !c.ProjectionMatrix().ApproxEqual(expected) {
 		t.Errorf("SetPosition failed\nexpected\n%s received\n%s", expected.String(), c.projectionMatrix.String())
 	}
 
 	c.SetZoom(5)
-	if !c.matrixDirty {
-		t.Errorf("SetZoom failed to set matrix dirty to true")
-	}
 	expected = mgl32.Mat4FromRows(mgl32.Vec4{0.1, 0, 0, -1}, mgl32.Vec4{0, -0.1, 0, 1}, mgl32.Vec4{0, 0, 0.5, 0}, mgl32.Vec4{0, 0, 0, 1})
 	if !c.ProjectionMatrix().ApproxEqual(expected) {
 		t.Errorf("SetZoom failed\nexpected\n%s received\n%s", expected.String(), c.projectionMatrix.String())
@@ -32,18 +26,12 @@ func TestCamera2D(t *testing.T) {
 	c := NewCamera2D(100, 100, 10, false)
 
 	c.SetPosition(10, 10)
-	if !c.matrixDirty {
-		t.Errorf("SetPosition failed to set matrix dirty to true")
-	}
 	expected := mgl32.Mat4FromRows(mgl32.Vec4{0.2, 0, 0, -3}, mgl32.Vec4{0, -0.2, 0, 3}, mgl32.Vec4{0, 0, 0.5, 0}, mgl32.Vec4{0, 0, 0, 1})
 	if !c.ProjectionMatrix().ApproxEqual(expected) {
 		t.Errorf("SetPosition failed\nexpected\n%s received\n%s", expected.String(), c.projectionMatrix.String())
 	}
 
 	c.SetZoom(5)
-	if !c.matrixDirty {
-		t.Errorf("SetZoom failed to set matrix dirty to true")
-	}
 	expected = mgl32.Mat4FromRows(mgl32.Vec4{0.1, 0, 0, -2}, mgl32.Vec4{0, -0.1, 0, 2}, mgl32.Vec4{0, 0, 0.5, 0}, mgl32.Vec4{0, 0, 0, 1})
 	if !c.ProjectionMatrix().ApproxEqual(expected) {
 		t.Errorf("SetZoom failed\nexpected\n%s received\n%s", expected.String(), c.projectionMatrix.String())
