dcf6a1b153bd5655352653e6a315eefae4e0dd77
Massimiliano Pesce <massimiliano.pesce@gmail.com>
2018-08-19 23:09:55 +0200

    Mouse is all float64 and handles the wheel

--- pkg/input/mouse.go
+++ pkg/input/mouse.go
@@ -10,21 +10,28 @@ import (
 var (
 	positionCallback glfw.CursorPosCallback
 	buttonCallback   glfw.MouseButtonCallback
+	scrollCallback   glfw.ScrollCallback
 
-	connected   bool
-	posX        int
-	posY        int
-	buttonsDown []bool
+	connected     bool
+	posX          float64
+	posY          float64
+	deltaX        float64
+	deltaY        float64
+	scrollOffsetX float64
+	scrollOffsetY float64
+	buttonsDown   []bool
 )
 
-func registerMouseCallbacks(pCallback glfw.CursorPosCallback, bCallback glfw.MouseButtonCallback) {
+func registerMouseCallbacks(pCallback glfw.CursorPosCallback, bCallback glfw.MouseButtonCallback, sCallback glfw.ScrollCallback) {
 	if positionCallback != nil {
 		fmt.Printf("Error: Mouse callbacks have been already registered!")
 	}
 	positionCallback = pCallback
 	buttonCallback = bCallback
+	scrollCallback = sCallback
 	app.GetWindow().SetCursorPosCallback(positionCallback)
 	app.GetWindow().SetMouseButtonCallback(buttonCallback)
+	app.GetWindow().SetScrollCallback(scrollCallback)
 }
 
 func unregisterMouseCallbacks() {
@@ -32,13 +39,16 @@ func unregisterMouseCallbacks() {
 	buttonCallback = nil
 	app.GetWindow().SetPosCallback(nil)
 	app.GetWindow().SetMouseButtonCallback(nil)
+	app.GetWindow().SetScrollCallback(nil)
 }
 
-// Registers the callbacks and starts receiving the mouse's events
+// ConnectMouse Registers the callbacks and starts receiving the mouse's events
 func ConnectMouse() {
 	pCallback := func(w *glfw.Window, x float64, y float64) {
-		posX = int(x)
-		posY = int(y)
+		deltaX = x - posX
+		deltaY = y - posY
+		posX = x
+		posY = y
 	}
 	bCallback := func(w *glfw.Window, button glfw.MouseButton, action glfw.Action, mod glfw.ModifierKey) {
 		if action == glfw.Press || action == glfw.Repeat {
@@ -47,31 +57,64 @@ func ConnectMouse() {
 			buttonsDown[button] = false
 		}
 	}
+	sCallback := func(w *glfw.Window, xoff float64, yoff float64) {
+		scrollOffsetX = xoff
+		scrollOffsetY = yoff
+	}
 
 	buttonsDown = make([]bool, glfw.MouseButtonLast, glfw.MouseButtonLast)
-	registerMouseCallbacks(pCallback, bCallback)
+	registerMouseCallbacks(pCallback, bCallback, sCallback)
 	connected = true
 }
 
-// Unregisters the callbacks and stops receiving the mouse's events
+// DisconnectMouse Unregisters the callbacks and stops receiving the mouse's events
 func DisconnectMouse() {
 	connected = false
 	unregisterMouseCallbacks()
 	buttonsDown = nil
 }
 
-// Returns the coordinates of the cursor's position
-func MousePosition() (int, int) {
+// MousePosition Returns the coordinates of the cursor's position
+func MousePosition() (float64, float64) {
 	if !connected {
-		return 0, 0
+		ConnectMouse()
 	}
 	return posX, posY
 }
 
-// Returns the state of the specified mouse button
+// MouseDelta Returns the latest movement of the cursor
+func MouseDelta() (float64, float64) {
+	if !connected {
+		ConnectMouse()
+	}
+
+	dX := deltaX
+	dY := deltaY
+
+	// The event has been consumed
+	deltaX = 0
+	deltaY = 0
+	return dX, dY
+}
+
+// MouseButton Returns the state of the specified mouse button
 func MouseButton(index int) bool {
 	if !connected {
-		return false
+		ConnectMouse()
 	}
 	return buttonsDown[index]
+}
+
+// MouseScroll Returns the last wheel's scroll offsets
+func MouseScroll() (float64, float64) {
+	if !connected {
+		ConnectMouse()
+	}
+	x, y := scrollOffsetX, scrollOffsetY
+
+	// The scroll event has been consumed
+	scrollOffsetX = 0
+	scrollOffsetY = 0
+
+	return x, y
 }
