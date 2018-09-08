c060f0f300970a45b4330d0fb5e159fdee82442a
Massimiliano Pesce <massimiliano.pesce@gmail.com>
2018-09-08 12:05:59 +0200

    Method 'set' of the mapping needs to be public

--- pkg/input/game_controller.go
+++ pkg/input/game_controller.go
@@ -70,7 +70,7 @@ type GameControllerMapping struct {
 	axes      []int
 }
 
-func (g *GameControllerMapping) set(nameRegEx string, buttons []int, axes []int) {
+func (g *GameControllerMapping) Set(nameRegEx string, buttons []int, axes []int) {
 	g.nameRegEx = nameRegEx
 	g.buttons = buttons
 	g.axes = axes
@@ -78,7 +78,7 @@ func (g *GameControllerMapping) set(nameRegEx string, buttons []int, axes []int)
 
 func init() {
 	// Keyboard as controller. This mapping should not be used by a JoystickController
-	MappingKeyboard.set("<None>",
+	MappingKeyboard.Set("<None>",
 		[]int{
 			int(glfw.KeyA), int(glfw.KeyS), int(glfw.KeyD), int(glfw.KeyF),
 			int(glfw.Key1), int(glfw.Key2), int(glfw.Key3),
@@ -91,10 +91,10 @@ func init() {
 	GameControllerMappings = make([]*GameControllerMapping, 0, 10)
 
 	// PS4 controller with USB cable (MacOS)
-	MappingPS4.set(".*Wireless Controller.*", []int{1, 2, 0, 3, 8, 12, 9, 10, 11, 4, 5, 14, 16, 17, 15, 13, 6, 7}, []int{0, 1, 2, 3, 4, 5})
+	MappingPS4.Set(".*Wireless Controller.*", []int{1, 2, 0, 3, 8, 12, 9, 10, 11, 4, 5, 14, 16, 17, 15, 13, 6, 7}, []int{0, 1, 2, 3, 4, 5})
 	GameControllerMappings = append(GameControllerMappings, &MappingPS4)
 
 	// XBox 360 wired controller (MacOS)
-	MappingXBox360.set(".*Xbox 360.*", []int{11, 12, 13, 14, 5, 10, 4, 6, 7, 8, 9, 0, 1, 2, 3}, []int{0, 1, 2, 3, 4, 5})
+	MappingXBox360.Set(".*Xbox 360.*", []int{11, 12, 13, 14, 5, 10, 4, 6, 7, 8, 9, 0, 1, 2, 3}, []int{0, 1, 2, 3, 4, 5})
 	GameControllerMappings = append(GameControllerMappings, &MappingXBox360)
 }
