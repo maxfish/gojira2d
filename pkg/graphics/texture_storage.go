bfff6f9edee12d0432c659e64b8490bc90e5f8f9
Massimiliano Pesce <massimiliano.pesce@gmail.com>
2018-09-15 09:50:02 +0200

    moved TextureStorage from utils to graphics

--- pkg/graphics/texture_storage.go
+++ pkg/graphics/texture_storage.go
@@ -1,32 +1,31 @@
-package utils
+package graphics
 
 import (
 	"fmt"
-	"github.com/maxfish/gojira2d/pkg/graphics"
 )
 
 // TextureStorage handles multiple textures, each one with its own id
 type TextureStorage struct {
 	path     string
-	textures map[string]*graphics.Texture
+	textures map[string]*Texture
 }
 
 // MakeTextureStorage creates a new initialized instance
 func MakeTextureStorage(path string) *TextureStorage {
 	return &TextureStorage{
 		path:     path,
-		textures: make(map[string]*graphics.Texture),
+		textures: make(map[string]*Texture),
 	}
 }
 
 // LoadTexture loads a texture from file and assigns an id to it
 func (ts *TextureStorage) LoadTexture(fileName string, textureId string) {
 	fullPath := fmt.Sprintf("%s/%s", ts.path, fileName)
-	t := graphics.NewTextureFromFile(fullPath)
+	t := NewTextureFromFile(fullPath)
 	ts.textures[textureId] = t
 }
 
 // TextureForId returns the texture associated with the id
-func (ts *TextureStorage) TextureForId(textureId string) *graphics.Texture {
+func (ts *TextureStorage) TextureForId(textureId string) *Texture {
 	return ts.textures[textureId]
 }
